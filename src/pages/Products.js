42e21bf0254ff68d1f3691e3c0ae3ce767b9a678
Marcus <vidranyimarcus@gmail.com>
2022-10-06 20:22:30 +0200

    design work

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -15,7 +15,7 @@ export function Products() {
 
 
     return (
-        <div className="products_container">
+        <div className="products_container" id="products">
             <div className="between_main_and_products_container">
                 <span>RENDELJEN MOST:</span><span style={{ color: "#A8894A" }}>edesmdesszert@gmail.com</span>
             </div>
