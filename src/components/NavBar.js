d8686d48e405539cefe331c13dedd3d966df83d6
Marcus <vidranyimarcus@gmail.com>
2022-10-07 02:38:31 +0200

    more design work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,15 +11,28 @@ export function NavBar() {
     }
 
     const scrollPosition = useScrollPosition();
-    
+
+    /* const lastLetterParisienne = (input) => {
+        return input.slice(-1);
+    } */
+
     return (
         <nav className="navbar-parent">
             <ul className={scrollPosition < 10 ? "navbar" : "nav_scrolled"}>
-                <li className="navbar-items"><Link to="main" spy={true} smooth={true} offset={-100} duration={500}>FŐOLDAL</Link></li>
-                <li className="navbar-items"><Link to="products" spy={true} smooth={true} offset={-100} duration={500}>KÍNÁLATUNK</Link></li>
-                <img src={edesm_logo} alt="edesm-logo" className="edesm-logo" id={scrollPosition < 10 ? "edesm_logo" : "edesm_logo_scrolled"} onClick={logoClickHandler} />
-                <li className="navbar-items"><Link to="aboutus" spy={true} smooth={true} offset={-100} duration={500}>RÓLUNK</Link></li>
-                <li className="navbar-items"><Link to="contact" spy={true} smooth={true} offset={0} duration={500}>ELÉRHETŐSÉG</Link></li>
+                <li className="navbar-items">
+                    <Link to="main" spy={true} smooth={true} offset={-100} duration={500}>Főolda<span className="lastletter">L</span></Link>
+                </li>
+                <li className="navbar-items">
+                    <Link to="products" spy={true} smooth={true} offset={-100} duration={500}>Kínálatun<span className="lastletter">K</span></Link>
+                </li>
+                <img src={edesm_logo} alt="edesm-logo" className="edesm-logo" id={scrollPosition < 10 ?
+                    "edesm_logo" : "edesm_logo_scrolled"} onClick={logoClickHandler} />
+                <li className="navbar-items">
+                    <Link to="aboutus" spy={true} smooth={true} offset={-100} duration={500}>Rólun<span className="lastletter">K</span></Link>
+                </li>
+                <li className="navbar-items">
+                    <Link to="contact" spy={true} smooth={true} offset={0} duration={500}>Elérhetősé<span className="lastletter">G</span></Link>
+                </li>
             </ul>
         </nav>
     )
