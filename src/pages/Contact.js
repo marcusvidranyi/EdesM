8bb05ac9e6a2864c7ae1ba4bc88070309fa0709c
Marcus <vidranyimarcus@gmail.com>
2022-12-01 05:03:24 +0100

    contact form integrated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faCircleUp
 } from "@fortawesome/free-solid-svg-icons";
+import React, { useRef } from 'react';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import emailjs from '@emailjs/browser';
 
 
 export function Contact() {
@@ -15,21 +19,45 @@ export function Contact() {
     }
 
 
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_mcdyvhq', 'template_s106rl8', form.current, '5XkFYpvBwSaEXEGXt')
+            .then((result) => {
+                console.log(result.text);
+                alert("üzenet elküldve!")
+            }, (error) => {
+                console.log(error.text);
+                alert("üzenetet nem sikerült elküldeni!")
+            });
+    };
+
     return (
         <>
             <div className="contact" id="contact">
-                <div className="contact_boxes">
+                <div className="contact_boxes" data-aos="fade-up">
                     <div className="contact_box_left">
                         <img src={edesm_logo} alt="edesm-logo" className="edesm-logo" />
                         <p>Elérhetőségek:</p>
                         <p style={{ color: "#FFFF" }}>ÉdesM Desszertműhely</p>
                         <p>2431 Perkáta, Dózsa György utca 22.</p>
                         <p style={{ color: "#FFFF" }}>Telefon: 06501332553, 06703240377</p>
-                        <p>Email: edesmdesszert@gmail.com</p>
+                        <a href="mailto:edesmdesszert@gmail.com" className='main-email'>Email: edesmdesszert@gmail.com</a>
+                        <h3 style={{color: "#FFFF"}}>Kérjük, hagyjön üzenetet!</h3>
+                        <form ref={form} onSubmit={sendEmail} className="contact_form">
+                            <label>Név</label>
+                            <input type="text" name="user_name" />
+                            <label>Email</label>
+                            <input type="email" name="user_email" />
+                            <label>Üzenet</label>
+                            <textarea name="message" />
+                            <input type="submit" value="Elküld" className="contact_form_submitbutton"/>
+                        </form>
                     </div>
                     <div className="contact_box_right">
 
-                        <div><div><iframe width="100%" height="800px" id="gmap_canvas" src="https://maps.google.com/maps?q=2431%20Perk%C3%A1ta%20D%C3%B3zsa%20Gy%C3%B6rgy%20utca%2022&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe></div></div>
+                        <div><div><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=2431%20Perk%C3%A1ta%20D%C3%B3zsa%20Gy%C3%B6rgy%20utca%2022&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe></div></div>
 
 
                     </div>
@@ -44,7 +72,7 @@ export function Contact() {
                             Gyakran ismételt kérdések
                         </Link>
                         <div>
-                            <FontAwesomeIcon icon={faCircleUp} className="tothetopbutton" onClick={clickToTheTop}/>
+                            <FontAwesomeIcon icon={faCircleUp} className="tothetopbutton" onClick={clickToTheTop} />
                         </div>
                         <Link to="/howto" className="footer_howto_link">
                             Rendelés menete
